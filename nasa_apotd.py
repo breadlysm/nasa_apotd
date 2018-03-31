2b511cb05d964c007f89ac54172d0a72045ba8b9
Breadlysm <me@breadlysm.com>
2018-03-31 10:45:18 -0400

    improved combatibility. Works across OSes and versions of python

--- nasa_apotd.py
+++ nasa_apotd.py
@@ -1,20 +1,50 @@
-import urllib
+try:
+    from urllib.request import urlretrieve
+    from urllib.request import urlopen  # Python 3
+except ImportError:
+    from urllib import urlretrieve
+    from urllib import urlopen  # Python 2
 from lxml import etree
 import os
 import datetime
+import errno
 
 now = datetime.datetime.now()
-url = "https://apod.nasa.gov/apod/astropix.html"
-response = urllib.urlopen(url)
-htmlparser = etree.HTMLParser()
-tree = etree.parse(response, htmlparser)
-folder = os.path.expanduser('~/Pictures/nasa/')
-imageurl = ('https://apod.nasa.gov/apod/'
-            + tree.xpath('/html/body/center[1]/p[2]/a/@href')[0]
-            )
-imagename = (folder
-             + now.strftime("%Y_%m_%d_")
-             + imageurl.split('/')[-1]
-             )
-urllib.urlretrieve(imageurl, imagename)
+base = "https://apod.nasa.gov/apod/"
+apotd_page = 'astropix.html'
+
+
+def get_folder(imgname):
+    user = os.path.expanduser('~')
+    pictures = os.path.join(user,'Pictures')
+    nasa = os.path.join(pictures, 'Nasa')
+    try:
+        os.makedirs(nasa)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    imgpath = os.path.join(pictures,'Nasa',imgname)
+    return imgpath
+
+
+
+def gettree(url):
+    response = urlopen(url)
+    htmlparser = etree.HTMLParser()
+    tree = etree.parse(response, htmlparser)
+    return tree
+
+# def download_past():
+
+def download_image(tree):
+    imageurl = (base
+                + tree.xpath('/html/body/center[1]/p[2]/a/@href')[0]
+                )
+    imagename = get_folder(now.strftime("%Y_%m_%d_") 
+                        + imageurl.split('/')[-1]
+                        )
+    urlretrieve(imageurl, imagename)
+
+download_image(gettree(base + apotd_page))
+
 
